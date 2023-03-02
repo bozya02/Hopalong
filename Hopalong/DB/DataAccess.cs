8273176c153e06d5fbf5271aae43637b8ae1dcee
bozya02 <bozinandrey1@yandex.ru>
2023-03-02 19:48:39 +0300

    Add project files.

---
+++ Hopalong/DB/DataAccess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopalong.DB
+{
+    public class DataAccess
+    {
+        public delegate void AddNewItem();
+        public static event AddNewItem AddNewItemEvent;
+
+        private static DbSet<Agent> _agents => HopalongEntities.GetContext().Agents;
+        private static DbSet<ProductSale> _productSales => HopalongEntities.GetContext().ProductSales;
+
+        public static List<Agent> GetAgents() => _agents.ToList();
+        public static List<AgentType> GetAgentTypes() => HopalongEntities.GetContext().AgentTypes.ToList();
+        public static List<Product> GetProducts() => HopalongEntities.GetContext().Products.ToList();
+
+
+        public static void SaveAgent(Agent agent)
+        {
+            if (agent.ID == 0)
+                _agents.Add(agent);
+
+            HopalongEntities.GetContext().SaveChanges();
+            AddNewItemEvent?.Invoke();
+        }
+
+        public static void DeleteAgent(Agent agent)
+        {
+            _agents.Remove(agent);
+            HopalongEntities.GetContext().SaveChanges();
+            AddNewItemEvent?.Invoke();
+        }
+
+        public static bool IsINNnKPPExist(Agent agent) => _agents.Any(x => x.ID != agent.ID && (x.INN == agent.INN || x.KPP == agent.KPP));
+
+
+        public static void DeleteProductSale(ProductSale productSale)
+        {
+            _productSales.Remove(productSale);
+            HopalongEntities.GetContext().SaveChanges();
+            AddNewItemEvent?.Invoke();
+        }
+    }
+}
---
+++ Hopalong/DB/HopalongEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hopalong.DB
+{
+    public partial class HopalongEntities
+    {
+        private static HopalongEntities _context;
+
+        public static HopalongEntities GetContext()
+        {
+            if (_context == null)
+                _context = new HopalongEntities();
+
+            return _context;
+        }
+    }
+}
